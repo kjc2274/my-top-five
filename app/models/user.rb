42f0a8d6d5f5015dbe5f986cc6678bc8f1618199
Kevin Cleary <kjc2274@yahoo.com>
2022-02-24 16:14:36 -0500

    user backend done

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
