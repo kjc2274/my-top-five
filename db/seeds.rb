46f90b5e5d0c35e5957264d2647781c17c284889
Kevin Cleary <kjc2274@yahoo.com>
2022-02-24 16:42:55 -0500

    categories backend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.destroy_all
+
+Category.create(title: "Movies", description: "", image: "");
+Category.create(title: "Books", description: "", image: "");
+Category.create(title: "Foods", description: "", image: "");
+Category.create(title: "Songs", description: "", image: "");
+Category.create(title: "TV Shows", description: "", image: "");
+Category.create(title: "Dream Destinations", description: "", image: "");
+Category.create(title: "Athletes", description: "", image: "");
+Category.create(title: "Artists", description: "", image: "");
+Category.create(title: "Vacation Spots", description: "", image: "");
+
+p "#{Category.all.count} categories created"
