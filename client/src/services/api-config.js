438179e75c789db8998ed392c44ffa92f8c4f7e6
Kevin Cleary <kjc2274@yahoo.com>
2022-02-25 15:36:43 -0500

    built category api routes

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -6,6 +6,7 @@ const api = axios.create({
     baseURL: baseUrl
 })
 
+// User api
 export const loginUser = async(loginData)=>{
     const res = await api.post('/auth/login', {authentication: loginData});
     localStorage.setItem('authToken', res.data.token);
@@ -45,3 +46,16 @@ export const deleteUser = async(user_id)=>{
     const res = await api.delete(`/users/${user_id}`);
     return res.data
 }
+
+// Category api
+export const getAllCategories = async()=>{
+    const res = await api.get('/categories');
+    return res.data
+}
+
+export const getOneCategory = async(category_id)=>{
+    const res = await api.get(`/categories/${category_id}`)
+    return res.data
+}
+
+// Review api
