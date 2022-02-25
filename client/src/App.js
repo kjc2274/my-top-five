45b26d300e377258239488f4425e5825f2e32261
Kevin Cleary <kjc2274@yahoo.com>
2022-02-25 15:29:34 -0500

    set up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { verifyUser } from './services/api-config';
 
 function App() {
+  //create currentUser variable for auth 
+  const [currentUser, setCurrentUser] = useState(null)
+  
+  //if there is an authToken in localStorage set that token's user as currentUser
+  useEffect(()=>{
+    const fetchUser = async()=>{
+      const user = await verifyUser();
+      setCurrentUser(user);
+    }
+    fetchUser();
+  },[])
+
+  // remove authToken from localStorage and reset currentUser
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    setCurrentUser(null);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route to/>
+        <Route/>
+        <Route/>
+      </Routes>
     </div>
   );
 }
