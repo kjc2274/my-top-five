37bda3cddb374064c3cc4c11edbd7595dc409462
Kevin Cleary <kjc2274@yahoo.com>
2022-02-25 20:26:09 -0500

    added input labels to signup and login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -21,8 +21,12 @@ export default function Login(props) {
         props.setCurrentUser(res);
         navigate('/')
     }}>
+      <label>Username:
         <input type='text' value={username} onChange={(e)=> setUsername(e.target.value)}/>
+      </label>
+      <label>Password:
         <input type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+      </label>
         <button>Login</button>
     </form>
   )
--- client/src/components/UserCreate/UserCreate.jsx
+++ client/src/components/UserCreate/UserCreate.jsx
@@ -7,6 +7,7 @@ export default function UserCreate(props) {
   const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
     const navigate = useNavigate();
 
   return (
@@ -17,13 +18,26 @@ export default function UserCreate(props) {
             email,
             password
         }
+        // prevent account creation if password does not match confirmPassword
+        if(password !== confirmPassword){
+         return alert("Password must match password confirmation!") 
+        }
         const res = await registerUser(user);
         props.setCurrentUser(res);
         navigate('/')
     }}>
+      <label>Username:
         <input type='text' value={username} onChange={(e)=> setUsername(e.target.value)}/>
+      </label>
+      <label>E-mail:  
         <input type='text' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+      </label>
+      <label>Password:
         <input type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+      </label>
+      <label>Confirm Password:
+        <input type='password' value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
+      </label>
         <button>Sign Up</button>
     </form>
   )
