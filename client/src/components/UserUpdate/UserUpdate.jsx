a34ae2ba0fed812a27fbee83253b9f8f0bfc664f
Kevin Cleary <kjc2274@yahoo.com>
2022-02-28 00:34:31 -0500

    UserUpdate styling

--- client/src/components/UserUpdate/UserUpdate.jsx
+++ client/src/components/UserUpdate/UserUpdate.jsx
@@ -1,4 +1,4 @@
-import './UserUpdate';
+import './UserUpdate.css';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { updateUser } from '../../services/api-config';
@@ -27,19 +27,21 @@ export default function UserUpdate(props) {
           props.setCurrentUser(res);        
           //navigate to UserDetail to confirm changes
           navigate(`/users/${id}`)
-      }}>
-        <label>Username:
-          <input type='text' value={username} onChange={(e)=> setUsername(e.target.value)}/>
-        </label>
-        <label>E-mail:  
-          <input type='text' value={email} onChange={(e)=> setEmail(e.target.value)}/>
-        </label>
-        <label>Password:
-          <input type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
-        </label>
-        <label>Confirm Password:
-          <input type='password' value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
-        </label>
+      }} id='user-update'>
+        <div id='user-edit-form'>
+          <label className='edit-input'>Username:
+            <input type='text' value={username} onChange={(e)=> setUsername(e.target.value)}/>
+          </label>
+          <label className='edit-input'>E-mail:  
+            <input type='text' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+          </label>
+          <label className='edit-input'>Password:
+            <input type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+          </label>
+          <label className='edit-input'>Confirm Password:
+            <input type='password' value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
+          </label>
+        </div>
           <button>Update</button>
       </form>
     )
