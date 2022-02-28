6963d5aeb64a9ffb5af806c74a788c613755fa4f
Kevin Cleary <kjc2274@yahoo.com>
2022-02-28 09:50:08 -0500

    added links to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,6 +2,12 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <h5 id='my-name'>made by Kevin Cleary</h5>
+      <div>
+        <a href="https://github.com/kjc2274" className='my-links'>Github</a>
+        <a href="https://www.linkedin.com/in/kevin-cleary-374a65228/" className='my-links'>LinkedIn</a>
+      </div>
+    </div>
   )
 }
