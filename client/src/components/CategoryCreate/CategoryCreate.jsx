58cd55f224b2fce60292aeabea5fc8dc54aabeb9
Kevin Cleary <kjc2274@yahoo.com>
2022-03-01 12:00:47 -0500

    built categoryCreate funtionality

--- client/src/components/CategoryCreate/CategoryCreate.jsx
+++ client/src/components/CategoryCreate/CategoryCreate.jsx
@@ -1,7 +1,40 @@
-import './CategoryCreate.css'
+import './CategoryCreate.css';
+import { useState } from 'react';
+import { createCategory } from '../../services/api-config';
+import { useNavigate } from 'react-router-dom';
 
 export default function CategoryCreate() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const navigate = useNavigate();
+
+
   return (
-    <div>CategoryCreate</div>
+    <form onSubmit={async(e)=>{
+      e.preventDefault();
+      const category = {
+          title,
+          description,
+          image
+      }
+      await createCategory(category);
+      navigate(`/categories`)
+      }}
+      id='cat-create-page'>
+       <h2 id='create-cat-title'>Create a Category!</h2>
+      <div id='cat-create-div'>
+        <label className='cat-create-form'>Title: 
+          <input type='text' value={title} onChange={(e)=> setTitle(e.target.value)} />
+        </label>
+        <label className='cat-create-form'>Description:   
+          <input type='text' value={description} onChange={(e)=> setDescription(e.target.value)} required/>
+        </label>
+        <label className='cat-create-form'>Image URL: 
+          <input type='text' value={image} onChange={(e)=> setImage(e.target.value)} required/>
+        </label>
+      </div>
+        <button id='create-cat-button'>Create</button>
+    </form>
   )
 }
