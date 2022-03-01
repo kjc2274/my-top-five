984c8447edda66ac22b7f78cea680cda2adc507b
Kevin Cleary <kjc2274@yahoo.com>
2022-03-01 11:11:50 -0500

    added a search bar to categories

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -4,22 +4,40 @@ import { getAllCategories } from '../../services/api-config';
 import { Link } from 'react-router-dom';
 
 export default function Categories() {
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
+  const [filteredCategories, setFilteredCategories] = useState([]);
+  const [q, setQ] = useState("");
 
   useEffect(()=>{
     const fetchCategories = async()=>{
       const res = await getAllCategories();
       setCategories(res);
+      setFilteredCategories(res)
     }
     fetchCategories();
   },[]);
+
+  const search = (e) =>{
+    // e.preventDefault();
+    const input = e.target.value;
+    if (input !== ''){
+        const filtered = categories.filter((category)=>{
+            return category.title.toLowerCase().includes(input.toLowerCase());
+        })
+        setFilteredCategories(filtered);
+    }else{
+        setFilteredCategories(categories);
+    }
+    setQ(input);
+}
 
   return (
     <div className='categories-page'>
       <h2 id='cat-page-title'>Categories</h2>
       <p className='cat-explain'>Select a category to see all of the lists posted under that category. See what other people enjoy, and maybe create your own list to share!</p>
+      <input type="search" value={q} onChange={search}/>
       <div className='cats'>
-        {categories.map((category)=>(
+        {filteredCategories.map((category)=>(
           <div className='cat-card'>
             <Link to={`/categories/${category.id}`} key={category.id}>
               <img src={category.image} alt={category.title} className='category-image'/>
