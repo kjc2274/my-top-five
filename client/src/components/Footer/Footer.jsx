2f8938ef6328bc256754a7913ed578e9cf0e1e09
Kevin Cleary <kjc2274@yahoo.com>
2022-03-14 20:13:36 -0400

    made footer links open in new window

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <div className='footer'>
       <h5 id='my-name'>made by Kevin Cleary</h5>
       <div>
-        <a href="https://github.com/kjc2274" className='my-links'>Github</a>
-        <a href="https://www.linkedin.com/in/kevin-cleary-374a65228/" className='my-links'>LinkedIn</a>
+        <a href="https://github.com/kjc2274" className='my-links' rel="noopener noreferrer" target="_blank">Github</a>
+        <a href="https://www.linkedin.com/in/kevin-cleary-374a65228/" className='my-links' rel="noopener noreferrer" target="_blank">LinkedIn</a>
       </div>
     </div>
   )
